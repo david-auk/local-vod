2ecd6a48e4b0bc97b7539f59dac5cacfc403bfec
david-auk <david@aukes.com>
2023-04-27 02:24:10 +0200

    initial functional version upload

---
+++ functions.py
@@ -0,0 +1,42 @@
+import subprocess
+import os
+from datetime import datetime
+
+def checkOrCreateDir(path):
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+def currentTime():
+	now = datetime.now()
+	hour = str(now.hour).zfill(2)
+	minute = str(now.minute).zfill(2)
+	day = str(now.day).zfill(2)
+	month = str(now.month).zfill(2)
+	year = now.year
+	hour_minute = str(hour) + ',' + str(minute)
+	formatted_time = '[' + hour_minute + ']-' + str(day) + '-' + str(month) + '-' + str(year)
+	return formatted_time
+
+def elapsedTime(start_time_str):
+	start_time = datetime.strptime(start_time_str, '%Y-%m-%dT%H:%M:%SZ')
+	end_time = datetime.now()
+	elapsed_time = end_time - start_time
+	elapsed_minutes = int(elapsed_time.total_seconds() // 60)
+	return elapsed_minutes
+
+def runInTmux(sessionName, command):
+	tmux_command = f'tmux new-session -d -s {sessionName} "{command}"'
+	subprocess.call(tmux_command, shell=True)
+
+def tmuxSessionRunning(sessionTargetName):
+	proc = subprocess.Popen(['tmux', 'list-sessions'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+	output = proc.communicate()[0].decode()
+
+	lines = output.splitlines()
+	for line in lines:
+
+		sessionName = line.split(':', 1)[0]
+		if sessionName == sessionTargetName:
+			return True
+
+	return False
---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import secret
+import functions
+
+# Replace with your Twitch client ID and client secret
+client_id = secret.twitchSecret['clientId']
+client_secret = secret.twitchSecret['clientSecret']
+downloadDir = secret.generalInfo['downloadDir']
+
+# Replace with the name of the Twitch channel you want to download
+channels = secret.twitchInfo['channels']
+
+for channel_name in channels:
+
+	if functions.tmuxSessionRunning(f'{channel_name}_dl-session'):
+		print(f'Already downloading stream of: {channel_name}')
+		quit()
+
+	# Get an OAuth token
+	oauth_url = f'https://id.twitch.tv/oauth2/token?client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials'
+	response = requests.post(oauth_url).json()
+	oauth_token = response['access_token']
+
+	# Get the current stream data
+	url = f'https://api.twitch.tv/helix/streams?user_login={channel_name}'
+	headers = {'Client-ID': client_id, 'Authorization': f'Bearer {oauth_token}', 'Accept': 'application/vnd.twitchtv.v5+json'}
+	response = requests.get(url, headers=headers).json()
+
+	# Check if the stream is live
+	if response['data']:
+
+		# Getting facts
+		channelNameClean = response['data'][0]['user_name']
+		startTime = response['data'][0]['started_at']
+
+		print(f'Starting download: {channel_name} {functions.elapsedTime(startTime)} minutes after stream started.')
+		
+		functions.checkOrCreateDir(f'{downloadDir}/{channel_name}')
+
+		filename = f'\'{downloadDir}/{channel_name}/{functions.currentTime()}.mp4\''#.replace("*", ",")
+		command = f'streamlink twitch.tv/{channel_name} best --twitch-api-header KEY={client_id} --twitch-disable-ads --stdout| ffmpeg -y -i - -c copy {filename}'
+
+		functions.runInTmux(f'{channel_name}_dl-session', command)
+	else:
+		print(f'{channel_name} is Offline..')
